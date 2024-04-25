11d12fc2dc3b3e9d4e1ca10ebda85139c643124d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-25 12:19:08 +0530

    Create S4  .java

---
+++ S4  .java
@@ -0,0 +1,33 @@
+package com.vijay.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		var ctx=SpringApplication.run(ExampleApplication.class, args);
+		MyFirstClass myFirstClass=ctx.getBean(MyFirstClass.class);
+		System.out.println(myFirstClass.sayHello() );
+	}
+
+	public MyFirstClass myFirstClass(){
+		return new MyFirstClass();
+	}
+}
+
+package com.vijay.example;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class MyFirstClass {
+
+    public String sayHello(){
+        return "Hello from the MyFirstClass";
+    }
+}
