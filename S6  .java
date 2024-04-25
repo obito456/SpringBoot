ad269711618e8f091249210c2f5d42b18c41699f
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-25 12:27:11 +0530

    Create S6  .java

---
+++ S6  .java
@@ -0,0 +1,43 @@
+package com.vijay.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		var ctx=SpringApplication.run(ExampleApplication.class, args);
+		MyFirstClass myFirstClass=ctx.getBean("myFirstClass",MyFirstClass.class);
+		System.out.println(myFirstClass.sayHello() );
+	}
+
+	public MyFirstClass myFirstClass(){
+		return new MyFirstClass();
+	}
+}
+
+package com.vijay.example;
+
+public class MyFirstClass {
+
+    public String sayHello(){
+        return "Hello from the MyFirstClass";
+    }
+}
+
+
+package com.vijay.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class ApplicationConfig {
+    @Bean
+    public MyFirstClass myFirstClass(){
+        return new MyFirstClass();
+    }
+}
