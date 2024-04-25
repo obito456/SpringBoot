705b71c0e5f980253f60b2120f1e87560a8e3fe1
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-04-25 13:02:25 +0530

    Create S10  .java

---
+++ S10  .java
@@ -0,0 +1,62 @@
+package com.vijay.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+		var ctx=SpringApplication.run(ExampleApplication.class, args);
+		MyFirstClass myFirstClass=ctx.getBean("myFirstBean",MyFirstClass.class);
+		System.out.println(myFirstClass.sayHello() );
+	}
+
+}
+
+package com.vijay.example;
+
+public class MyFirstClass {
+
+    private String myVar;
+
+    public MyFirstClass (String myVar) {
+        this.myVar = myVar;
+    }
+
+    public String sayHello(){
+        return "Hello from the MyFirstClass ==> myVar = " + myVar;
+    }
+}
+
+package com.vijay.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+
+public class ApplicationConfig {
+    @Bean
+
+    public MyFirstClass myFirstBean(){
+        return new MyFirstClass("myFirstBean");
+    }
+
+}
+
+package com.vijay.example;
+
+import  org.springframework.stereotype.Service;
+
+@Service
+
+public class MyFirstService {
+
+    private MyFirstClass myFirstClass;
+
+    public String tellAStory(){
+        return "the dependency is saying :"+ myFirstClass.sayHello();
+    }
+}
