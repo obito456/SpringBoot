011a814d2e965171a2dadcf3ca0f244df67bbcfe
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 03:52:58 +0530

    Update and rename S7  .java to S7  Configuration .java

--- S7  Configuration .java
+++ S7  Configuration .java
@@ -1,41 +1,31 @@
 package com.vijay.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ExampleApplication {
-
 	public static void main(String[] args) {
 		var ctx=SpringApplication.run(ExampleApplication.class, args);
-		MyFirstClass myFirstClass=ctx.getBean("myBean",MyFirstClass.class);
+		MyFirstClass myFirstClass=ctx.getBean("beanone",MyFirstClass.class);
 		System.out.println(myFirstClass.sayHello() );
-	}
-
-	public MyFirstClass myFirstClass(){
-		return new MyFirstClass();
 	}
 }
 
 package com.vijay.example;
 
 public class MyFirstClass {
-
     public String sayHello(){
         return "Hello from the MyFirstClass";
     }
 }
 
 package com.vijay.example;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ApplicationConfig {
-    @Bean("myBean")
+    @Bean("beanone")
     public MyFirstClass myFirstClass(){
         return new MyFirstClass();
     }
