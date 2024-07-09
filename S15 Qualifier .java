a7446e945b96ebf7cddf474cd74d6247269298ea
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 04:44:19 +0530

    Update and rename S15  .java to S15 Qualifier .java

---
+++ S15 Qualifier .java
@@ -0,0 +1,59 @@
+package com.example.vijay;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VijayApplication {
+	public static void main(String[] args) {
+		var ctx=SpringApplication.run(VijayApplication.class, args);
+		Myservice myservice=ctx.getBean(Myservice.class);
+		System.out.println(myservice.tellstory());
+	}
+}
+
+package com.example.vijay;
+
+public class Firstclass {
+    private String name;
+    public Firstclass(String name){
+        this.name=name;
+    }
+    public String greets(){
+        return "hi hello bye "+name;
+    } 
+}
+
+package com.example.vijay;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class Appconfig {
+    @Bean
+    public Firstclass myfirstclass(){
+		return new Firstclass("vijaykumar");
+    }
+    @Bean
+    public Firstclass mysecondlass(){
+		return new Firstclass("vipparthi");
+    }
+    @Bean
+    public Firstclass mythirdlass(){
+		return new Firstclass("boom boom");
+    }   
+}
+
+package com.example.vijay;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Myservice {
+    @Autowired
+    @Qualifier("mysecondclass")
+    private Firstclass firstobj;
+    public String tellstory(){
+        return "story is "+firstobj.greets();
+    }
+}
