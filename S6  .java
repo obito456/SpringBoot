af6fe478e0bcc2d7a5dfc5c826a52067e8657967
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 03:49:18 +0530

    Update S6  .java

--- S6  .java
+++ S6  .java
@@ -1,27 +1,20 @@
 package com.vijay.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
 		var ctx=SpringApplication.run(ExampleApplication.class, args);
-		MyFirstClass myFirstClass=ctx.getBean("myFirstClass",MyFirstClass.class);
+		MyFirstClass myFirstClass=ctx.getBean("firstClass",MyFirstClass.class);
 		System.out.println(myFirstClass.sayHello() );
-	}
-
-	public MyFirstClass myFirstClass(){
-		return new MyFirstClass();
 	}
 }
 
 package com.vijay.example;
 
 public class MyFirstClass {
-
     public String sayHello(){
         return "Hello from the MyFirstClass";
     }
@@ -29,15 +22,13 @@ public class MyFirstClass {
 
 
 package com.vijay.example;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ApplicationConfig {
     @Bean
-    public MyFirstClass myFirstClass(){
+    public MyFirstClass firstClass(){
         return new MyFirstClass();
     }
 }
