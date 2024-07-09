560df54ebbe0ca8168d5787a7486ae062a72a89e
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 04:16:03 +0530

    Update and rename S11  .java to S11 DI .java

--- S11 DI .java
+++ S11 DI .java
@@ -1,44 +1,34 @@
 package com.vijay.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ExampleApplication {
-
 	public static void main(String[] args) {
 		var ctx=SpringApplication.run(ExampleApplication.class, args);
-		MyFirstService myFirstService=ctx.getBean("myFirstBean",MyFirstService.class);
+		MyFirstService myFirstService=ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService  .tellAStory() );
 	}
-
 }
 
 package com.vijay.example;
 
 public class MyFirstClass {
-
     private String myVar;
-
     public MyFirstClass (String myVar) {
         this.myVar = myVar;
     }
-
     public String sayHello(){
-        return "Hello from the MyFirstClass ==> myVar = " + myVar;
+        return "Hello  = " + myVar;
     }
 }
 
 package com.vijay.example;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import  org.springframework.stereotype.Service;
 
 @Service
-
 public class   MyFirstService {
-
     private final MyFirstClass myFirstClass;
 
     @Autowired
@@ -52,15 +42,12 @@ public class   MyFirstService {
 }
 
 package com.vijay.example;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ApplicationConfig {
     @Bean
-
     public MyFirstClass myFirstBean(){
         return new MyFirstClass("myFirstBean");
     }
