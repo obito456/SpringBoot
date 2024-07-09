558858b6079194cd044b4c1f28f9d98e7542828d
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 03:57:46 +0530

    Update and rename S8  .java to S8  Configuration .java

--- S8  Configuration .java
+++ S8  Configuration .java
@@ -1,20 +1,13 @@
 package com.vijay.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ExampleApplication {
-
 	public static void main(String[] args) {
 		var ctx=SpringApplication.run(ExampleApplication.class, args);
 		MyFirstClass myFirstClass=ctx.getBean("myFirstBean",MyFirstClass.class);
 		System.out.println(myFirstClass.sayHello() );
-	}
-
-	public MyFirstClass myFirstClass(){
-		return new MyFirstClass();
 	}
 }
 
@@ -22,18 +15,17 @@ package com.vijay.example;
 
 public class MyFirstClass {
 
+    private String name="vijay";	
     public String sayHello(){
-        return "Hello from the MyFirstClass";
+        return "Hello from the MyFirstClass "+name;
     }
 }
 
 package com.vijay.example;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ApplicationConfig {
     @Bean
     public MyFirstClass myFirstBean(){
