354af953a015567184c9586363d070dbca3c3ec0
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 04:25:53 +0530

    Update and rename S12  .java to S12  Qualifier .java

--- S12  Qualifier .java
+++ S12  Qualifier .java
@@ -1,34 +1,31 @@
 package com.vijay.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ExampleApplication {
-
 	public static void main(String[] args) {
 		var ctx=SpringApplication.run(ExampleApplication.class, args);
-		MyFirstService myFirstService=ctx.getBean("myFirstBean",MyFirstService.class);
+		MyFirstService myFirstService=ctx.getBean(,MyFirstService.class);
 		System.out.println(myFirstService  .tellAStory() );
 	}
-
 }
 
 package com.vijay.example;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ApplicationConfig {
     @Bean
-
-    public MyFirstClass myFirstBean(){
-        return new MyFirstClass("myFirstBean");
+    @Qualifier("fir")
+    public Firstclass myfirstclass(){
+		return new Firstclass("vijaykumar");
     }
-
+    @Bean
+    @Qualifier("sec")
+    public Firstclass mysecondlass(){
+		return new Firstclass("vipparthi");
 }
 
 package com.vijay.example;
@@ -36,27 +33,22 @@ package com.vijay.example;
 public class MyFirstClass {
 
     private String myVar;
-
     public MyFirstClass (String myVar) {
         this.myVar = myVar;
     }
-
     public String sayHello(){
         return "Hello from the MyFirstClass ==> myVar = " + myVar;
     }
 }
 
 package com.vijay.example;
-
 import  org.springframework.stereotype.Service;
 
 @Service
-
 public class   MyFirstService {
-
     private final MyFirstClass myFirstClass;
 
-    public MyFirstService(MyFirstClass myFirstClass) {
+    public MyFirstService(@Qualifier("sec") MyFirstClass myFirstClass) {
         this.myFirstClass = myFirstClass;
     }
 
