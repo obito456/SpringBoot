2f3b16b57054706972c253869ee9204136b9d409
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-07-10 04:01:03 +0530

    Update and rename S9  .java to S9  Configuration .java

--- S9  Configuration .java
+++ S9  Configuration .java
@@ -1,48 +1,38 @@
 package com.vijay.example;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class ExampleApplication {
-
 	public static void main(String[] args) {
 		var ctx=SpringApplication.run(ExampleApplication.class, args);
 		MyFirstClass myFirstClass=ctx.getBean("myFirstBean",MyFirstClass.class);
 		System.out.println(myFirstClass.sayHello() );
 	}
-
 }
 
 package com.vijay.example;
 
 public class MyFirstClass {
-
-    private String myVar;
-
-    public MyFirstClass (String myVar) {
-        this.myVar = myVar;
+	
+    private String name;
+    public MyFirstClass (String name) {
+        this.name=name;
     }
-
     public String sayHello(){
-        return "Hello from the MyFirstClass ==> myVar = " + myVar;
+        return "Hello " + name;
     }
 }
 
-
 package com.vijay.example;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-
 public class ApplicationConfig {
     @Bean
-
     public MyFirstClass myFirstBean(){
-        return new MyFirstClass("myFirstBean");
+        return new MyFirstClass("vijay");
     }
 
 }
