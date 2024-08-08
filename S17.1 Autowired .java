09903b5470a734a813924758ae9eef432cf7010e
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-08 19:00:13 +0530

    Create S17.1 Autowired .java

---
+++ S17.1 Autowired .java
@@ -0,0 +1,31 @@
+package com.example.vijay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VijayApplication {
+
+    public static void main(String[] args) {
+        var ctx=SpringApplication.run(VijayApplication.class, args);
+        Driver driver =ctx.getBean(Driver.class);
+        driver.getJavaVersion();
+    }
+}
+package com.example.vijay;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class Driver {
+    @Autowired
+    public Environment environment;
+
+    public void getJavaVersion() {
+        System.out.println("Java Version: " + environment.getProperty("java.version"));
+    }
+}
