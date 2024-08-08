269d46d71fe4f955fc997b83602030ad91143ea0
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-08 17:25:59 +0530

    Create S07.1 Configuration .java

---
+++ S07.1 Configuration .java
@@ -0,0 +1,47 @@
+package com.example.vijay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VijayApplication {
+
+    public static void main(String[] args) {
+        var ctx=SpringApplication.run(VijayApplication.class, args);
+		Car car1=ctx.getBean("audi",Car.class);
+		car1.display();
+    }
+}
+
+package com.example.vijay;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Car {
+    private String name;
+    public void display(){
+        System.out.println("car name : "+name);
+    }
+}
+
+package com.example.vijay;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean("ferrari")
+    public Car car1(){
+        return new Car("Ferrari");
+    }
+    @Bean("audi")
+    public Car car2(){
+        return new Car("audi");
+    }
+}
