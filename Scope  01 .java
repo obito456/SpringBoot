274089f72c276bf715e0798d8a431dfad4501488
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-31 18:22:36 +0530

    Update Scope  01 .java

--- Scope  01 .java
+++ Scope  01 .java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 
 @Component
 @Scope("prototype")
