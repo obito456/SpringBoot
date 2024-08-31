8216476b06d8c1b029d93a2827fd324590e93987
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-31 19:03:24 +0530

    Update Scope 03 .java

--- Scope 03 .java
+++ Scope 03 .java
@@ -3,12 +3,16 @@ package com.example.Solution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Component
+@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
 class Audi {
     public Audi() {
         System.out.println("Audi instance created");
@@ -19,8 +23,9 @@ class Audi {
 @RequestMapping("/car")
 class Car {
     public Car() {
-        System.out.println("car is created");
+        System.out.println("Car is created");
     }
+
     @Autowired
     private Audi audi;
 
@@ -34,6 +39,6 @@ class Car {
 @SpringBootApplication
 public class SolutionApplication {
     public static void main(String[] args) {
-        var ctx=SpringApplication.run(SolutionApplication.class, args);
+        SpringApplication.run(SolutionApplication.class, args);
     }
 }
