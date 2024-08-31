9fb0fe5f1c5908f82b4c28f636129cfa788a8745
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-31 13:55:59 +0530

    Create DI  01 .java

---
+++ DI  01 .java
@@ -0,0 +1,33 @@
+package com.example.Solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+class Car {
+    public void drive(String name) {
+        System.out.println(" I will to travel on "+ name);
+    }
+}
+
+class Person {
+    private Car car;
+
+    public Person(Car car) {
+        this.car=car;
+	}
+
+    public void favourite(String name) {
+        car.drive(name);
+    }
+}
+
+@SpringBootApplication
+public class SolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionApplication.class, args);
+		Car car1=new Car();
+		Person Person=new Person(car1);
+		Person.favourite("Ferrari");
+	}
+}
