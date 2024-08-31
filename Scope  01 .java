a001a43cfc14d5cd86da75dfd6ebf7e4b8e6bac1
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-31 18:22:06 +0530

    Create Scope  01 .java

---
+++ Scope  01 .java
@@ -0,0 +1,26 @@
+package com.example.Solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+@Component
+@Scope("prototype")
+class Car {
+    public Car() {
+        System.out.println("car is created");
+    }
+}
+
+@SpringBootApplication
+public class SolutionApplication {
+    public static void main(String[] args) {
+        var ctx=SpringApplication.run(SolutionApplication.class, args);
+        Car car1=ctx.getBean(Car.class);
+        Car car2=ctx.getBean(Car.class);
+        System.out.println("Are both cars the same? " + (car1 == car2));
+    }
+}
