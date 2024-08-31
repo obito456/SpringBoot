511bfe559d48083f715d3458b7296ac0f186bb14
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-08-31 18:26:23 +0530

    Create Scope 02 .java

---
+++ Scope 02 .java
@@ -0,0 +1,22 @@
+package com.example.Solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+class Car {
+    public Car() {
+        System.out.println("car is created");
+    }
+}
+
+@SpringBootApplication
+public class SolutionApplication {
+    public static void main(String[] args) {
+        var ctx=SpringApplication.run(SolutionApplication.class, args);
+        Car car1=ctx.getBean(Car.class);
+        Car car2=ctx.getBean(Car.class);
+        System.out.println("Are both cars the same? " + (car1 == car2));
+    }
+}
