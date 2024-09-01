d3f04d1089323e416792f8bbb299607ca81f6a79
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-09-01 13:15:01 +0530

    Create Bean 02 .java

---
+++ Bean 02 .java
@@ -0,0 +1,41 @@
+package com.example.Solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Horse {
+    private String breed;
+    private String color;
+}
+
+@SpringBootApplication
+public class SolutionApplication {
+
+    @Bean
+    public Horse horse1() {
+        return new Horse("Arabian", "White");
+    }
+
+    @Bean("obj")
+    public Horse horse2() {
+        return new Horse("Freisian", "Black");
+    }
+
+    public static void main(String[] args) {
+        var ctx = SpringApplication.run(SolutionApplication.class, args);
+        Horse horse = ctx.getBean("obj",Horse.class);
+        System.out.println("Horse breed: " + horse.getBreed());
+        System.out.println("Horse color: " + horse.getColor());
+    }
+}
