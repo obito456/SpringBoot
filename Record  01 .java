a510db2d269a0171503749db7d8549674981f63b
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-15 16:28:12 +0530

    Create Record  01 .java

---
+++ Record  01 .java
@@ -0,0 +1,39 @@
+package com.example.Solution;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    @JsonProperty("cn")
+    String customerName;
+    @JsonProperty("fd")
+    String Food;
+    @JsonProperty("qt")
+    int quantity;
+}
+
+record OrderRecord(String customerName,String Food,int quantity) {
+}
+
+@RestController
+@RequestMapping("/zomato")
+class Zomato{
+
+    @PostMapping("/order")
+    public void run(@RequestBody Order order) {
+        System.out.println("You ordered "+order);
+    }
+    @PostMapping("/rorder")
+    public void run(@RequestBody OrderRecord order) {
+        System.out.println("You ordered "+order);
+    }
+}
