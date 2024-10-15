1834645eb62f3809175e191543549cabaecd539c
vijayuchiha <150343458+obito456@users.noreply.github.com>
2024-10-15 11:49:33 +0530

    Create Getmapping 01 .java

---
+++ Getmapping 01 .java
@@ -0,0 +1,16 @@
+package com.example.Solution;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/car")
+public class Car {
+
+    @GetMapping("/run")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void run() {
+        System.out.println("Car is running");
+    }
+}
